2d54a53af16597f5aa4bd46b8c13c1a3ab5692af
dolopihtis <dolopihtiss@gmail.com>
2017-05-28 18:37:07 +0300

    работающий помидор

--- app/main.ts
+++ app/main.ts
@@ -1,24 +1,67 @@
 import { Component, NgModule } from '@angular/core';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-// Our first "Hello world" component
+// Pomodoro timer component
 @Component({
-  selector: 'hello-angular',
-  template: '<h1 class="text-center"> {{greeting}} </h1>'
+    selector: 'pomodoro-timer',
+    template: `
+    <div class="text-center">
+      <img src="./assets/img/pomodoro.png" alt="Pomodoro">
+      <h1> {{ minutes }}:{{ seconds | number: '2.0' }} </h1>
+      <p>
+        <button (click)="togglePause()"
+          class="btn btn-danger">
+          {{ buttonLabel }}
+        </button>
+      </p>
+    </div>
+`
 })
-export class HelloAngularComponent {
-  greeting: string;
-  constructor() {
-    this.greeting = 'Hello Angular!';
-  }
+export class PomodoroTimerComponent {
+    minutes: number;
+    seconds: number;
+    isPaused: boolean;
+    buttonLabel: string;
+
+    constructor() {
+        this.resetPomodoro();
+        setInterval(() => this.tick(), 1000);
+    }
+
+    resetPomodoro(): void {
+        this.isPaused = true;
+        this.minutes = 24;
+        this.seconds = 59;
+        this.buttonLabel = 'Start';
+    }
+
+    private tick(): void {
+        if (!this.isPaused) {
+            this.buttonLabel = 'Pause';
+
+            if (--this.seconds < 0) {
+                this.seconds = 59;
+                if (--this.minutes < 0) {
+                    this.resetPomodoro();
+                }
+            }
+        }
+    }
+
+    togglePause(): void {
+        this.isPaused = !this.isPaused;
+        if (this.minutes < 24 || this.seconds < 59) {
+            this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+        }
+    }
 }
 
-// Main module, bootstrapping HelloAngularComponent as root component
+// Main module, bootstrapping PomodoroTimerComponent as root component
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [HelloAngularComponent],
-  bootstrap: [HelloAngularComponent],
+    imports: [BrowserModule],
+    declarations: [PomodoroTimerComponent],
+    bootstrap: [PomodoroTimerComponent],
 })
 export class AppModule { }
 
